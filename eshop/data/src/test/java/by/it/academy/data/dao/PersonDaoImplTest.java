77e75fd4022f960e11b5a9ff43d3a45341a88090
Aliaksandr Veramkovich <averamkovich@allot.com>
2023-11-14 08:58:11 +0300

    Lesson 19: Hibernate object mapping One-to-Many and Many-to-One - unit test

--- eshop/data/src/test/java/by/it/academy/data/dao/PersonDaoImplTest.java
+++ eshop/data/src/test/java/by/it/academy/data/dao/PersonDaoImplTest.java
@@ -4,6 +4,7 @@ import by.it.academy.data.EShopTestDataSource;
 import by.it.academy.data.EShopTestSessionFactory;
 import by.it.academy.data.pojo.Person;
 import by.it.academy.data.pojo.PersonDetails;
+import by.it.academy.data.pojo.TargetGroup;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -23,18 +24,19 @@ public class PersonDaoImplTest {
     public void setUp() throws Exception {
         personDao = new PersonDaoImpl(EShopTestSessionFactory.getSessionFactory());
         Connection conn = EShopTestDataSource.getConnection();
-        conn.createStatement().executeUpdate(
-                "truncate table T_PERSON;"
-        );
+        conn.createStatement().executeUpdate("DELETE FROM T_PERSON;");
+        conn.createStatement().executeUpdate("DELETE FROM T_TARGET_GROUP;");
+        conn.createStatement().executeUpdate("DELETE FROM T_PERSON_DETAILS;");
+
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
         personDao = null;
         Connection conn = EShopTestDataSource.getConnection();
-        conn.createStatement().executeUpdate(
-                "truncate table T_PERSON;"
-        );
+        conn.createStatement().executeUpdate("DELETE FROM T_PERSON;");
+        conn.createStatement().executeUpdate("DELETE FROM T_TARGET_GROUP;");
+        conn.createStatement().executeUpdate("DELETE FROM T_PERSON_DETAILS;");
     }
 
     @Test
@@ -59,6 +61,35 @@ public class PersonDaoImplTest {
         rs.next();
         int actualCount = rs.getInt(1);
         assertEquals(1, actualCount);
+    }
+
+    @Test
+    public void testSaveNewPersonWithTargetGroup() throws SQLException, ClassNotFoundException {
+        // Given
+        Connection conn = EShopTestDataSource.getConnection();
+        String targetGroupUUID = "b5bed480-507b-4e5d-a33e-f3ce37af0000";
+        conn.createStatement().executeUpdate("INSERT INTO T_TARGET_GROUP (ID, GROUP_NAME) VALUES('"
+                + targetGroupUUID
+                + "', 'TestTargetGroup');");
+        Person person = new Person(
+                null, "John", "SmithWithTargetGroup", new Date()
+        );
+        TargetGroup targetGroup = EShopTestSessionFactory
+                .getSessionFactory().openSession().get(TargetGroup.class, targetGroupUUID);
+        person.setTargetGroup(targetGroup);
+
+        // When
+        String savedId = personDao.saveNewPerson(person);
+
+        //Then
+        ResultSet rs = conn.createStatement().executeQuery(
+                "SELECT TARGET_GROUP_ID from T_PERSON where PERSON_ID='" + savedId + "';"
+        );
+        rs.next();
+        String savedGroupId = rs.getString(1);
+        assertNotNull(savedGroupId);
+        assertEquals(targetGroupUUID, savedGroupId);
+        conn.close();
     }
 
     @Test
